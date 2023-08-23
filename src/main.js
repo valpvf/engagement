e5a99b1b204bac5ad1351b5298fab581fa785f9f
Valerii Pozhydaiev <p_vf@i.ua>
2023-08-23 16:32:44 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -9,49 +9,50 @@ import '/scss/selections.scss';
 import '/scss/footer.scss';
 import { getDataApi } from './servises/productsApi';
 
-const mainData = await getDataApi().then(res => res.products);
-const images = mainData.map(el => el.mainImage);
-console.log('mainData', mainData);
-console.log('images', images);
+window.onload = async function () {
+  const mainData = await getDataApi().then(res => res.products);
+  const images = mainData.map(el => el.mainImage);
+  console.log('mainData', mainData);
+  console.log('images', images);
 
-// window.onload = async function () {
-// Получаем данные из JSON-файла
-// const data = await fetch(mainData).then(response => response.json());
+  // window.onload = async function () {
+  // Получаем данные из JSON-файла
+  // const data = await fetch(mainData).then(response => response.json());
 
-// Получаем массив изображений
-// const images = data.mainImage;
+  // Получаем массив изображений
+  // const images = data.mainImage;
 
-// Определяем количество изображений
-const totalSlides = images.length;
+  // Определяем количество изображений
+  const totalSlides = images.length;
 
-// Определяем количество видимых слайдов (минимум 4, максимум 16)
-let slidesToShow = Math.min(totalSlides, 16);
-slidesToShow = Math.max(slidesToShow, 2);
+  // Определяем количество видимых слайдов (минимум 4, максимум 16)
+  let slidesToShow = Math.min(totalSlides, 16);
+  slidesToShow = Math.max(slidesToShow, 2);
 
-// Динамически создаем элементы слайдов и добавляем их в слайдер
-const slider = document.querySelector('.slider');
-for (let i = 0; i < totalSlides; i++) {
-  const slide = document.createElement('div');
-  slide.innerHTML = `
+  // Динамически создаем элементы слайдов и добавляем их в слайдер
+  const slider = document.querySelector('.slider');
+  for (let i = 0; i < totalSlides; i++) {
+    const slide = document.createElement('div');
+    slide.innerHTML = `
     <img src="${images[i]}" alt="Image ${i + 1} class="exclusive-img">
   `;
-  slider.appendChild(slide);
-}
+    slider.appendChild(slide);
+  }
 
-// Инициализируем слайдер
-const slickOptions = {
-  slidesToShow: slidesToShow,
-  slidesToScroll: 1,
-  autoplay: true,
-  autoplaySpeed: 1500,
-  infinite: true,
-  variableWidth: true,
-  centerMode: true,
-  prevArrow: document.querySelector('.prev-btn'),
-  nextArrow: document.querySelector('.next-btn'),
+  // Инициализируем слайдер
+  const slickOptions = {
+    slidesToShow: slidesToShow,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    infinite: true,
+    variableWidth: true,
+    centerMode: true,
+    prevArrow: document.querySelector('.prev-btn'),
+    nextArrow: document.querySelector('.next-btn'),
+  };
+  slider.slick(slickOptions);
 };
-slider.slick(slickOptions);
-// };
 
 // import javascriptLogo from "../javascript.svg";
 // import viteLogo from "../public/favicon/vite.svg";
