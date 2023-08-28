3df05136993c48bbdfc98ccf67347caeb818971b
Artem-Grushchinskiy <122301492+Artem-Grushchinskiy@users.noreply.github.com>
2023-08-28 22:13:02 +0200

    footer gradient

--- src/js/social-media.js
+++ src/js/social-media.js
@@ -1,27 +1,36 @@
 $('.ring').hover(function () {
   $('.footer').toggleClass('color-ring');
+  $('.footer').toggleClass('footer-gradient');
 });
 $('.facebook').hover(function () {
   $('.footer').toggleClass('color-facebook');
+  $('.footer').toggleClass('footer-gradient');
 });
 $('.email').hover(function () {
   $('.footer').toggleClass('color-email');
+  $('.footer').toggleClass('footer-gradient');
 });
 $('.telegram').hover(function () {
   $('.footer').toggleClass('color-telegram');
+  $('.footer').toggleClass('footer-gradient');
 });
 $('.phone').hover(function () {
   $('.footer').toggleClass('color-phone');
+  $('.footer').toggleClass('footer-gradient');
 });
 $('.instagram').hover(function () {
   $('.footer').toggleClass('color-instagram');
+  $('.footer').toggleClass('footer-gradient');
 });
 $('.whatsapp').hover(function () {
   $('.footer').toggleClass('color-whatsapp');
+  $('.footer').toggleClass('footer-gradient');
 });
 $('.viber').hover(function () {
   $('.footer').toggleClass('color-viber');
+  $('.footer').toggleClass('footer-gradient');
 });
 $('.tiktok').hover(function () {
   $('.footer').toggleClass('color-tiktok');
+  $('.footer').toggleClass('footer-gradient');
 });
