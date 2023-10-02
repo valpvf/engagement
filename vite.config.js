6973a034ba25496aa53e509307ca18eff1ec7b2e
Valerii Pozhydaiev <p_vf@i.ua>
2023-10-02 19:59:11 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,19 +3,19 @@ import { defineConfig } from 'vite';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
 // import handlebars from 'vite-plugin-handlebars';
-// import { resolve } from 'path'
+import { resolve } from 'path';
 
 export default defineConfig({
   root: 'src',
   build: {
-    // rollupOptions: {
-    //   input: glob.sync("./src/*.html"),
-    //   // input: {
-    //   //   input: glob.sync("./src/*.html"),
-    //   //   main: resolve(__dirname, "./src/index.html"),
-    //   //   comics: resolve(__dirname, './src/comics/index.html'),
-    //   // },
-    // },
+    rollupOptions: {
+      //   input: glob.sync("./src/*.html"),
+      input: {
+        //   input: glob.sync("./src/*.html"),
+        main: resolve(__dirname, 'src/index.html'),
+        error: resolve(__dirname, 'src/404.html'),
+      },
+    },
     outDir: '../dist',
   },
   plugins: [
